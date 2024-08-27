3f185bfe85ff468562bbd5f0cdbc84d7ed5ee7e2
octopus198 <linhtran190897@gmail.com>
2024-08-27 22:10:13 +0700

    test deploy

--- src/components/formComponents/QuillTextEditor.tsx
+++ src/components/formComponents/QuillTextEditor.tsx
@@ -1,72 +1,80 @@
 import React from "react";
 import { Controller } from "react-hook-form";
-import ReactQuill from "react-quill";
+import ReactQuill, { Quill } from "react-quill";
 
-const QuillTextEditor = ({
-  language,
-  control,
-  errors,
-  name,
-  property,
-}) => {
+import ImageUploader from "quill-image-uploader";
+import { uploadFile } from "../../api/upload";
+
+Quill.register("modules/imageUploader", ImageUploader);
+
+const QuillModules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    ["link"],
+    [{ color: [] }, { background: [] }],
+    ["image"],
+    [{ align: [] }],
+  ],
+  imageUploader: {
+    upload: async (file) => {
+      const response = await uploadFile(file);
+      return response.data.data.fileUrl;
+    },
+  },
+};
+
+const QuillTextEditor = ({ language, control, errors, name, property }) => {
   const fieldName = `localeData[${language}][${property}]`;
   const fieldError = errors?.localeData?.[language]?.[property];
+
   return (
     <Controller
-    name={fieldName}
-    key={fieldName}
-    control={control}
-    defaultValue=""
-    rules={{
-      required: `${name} is required`,
-      validate: {
-        notEmptyOrWhitespace: (value) => {
-          const textContent = value.replace(/<[^>]*>/g, "").trim();
-          if (!textContent) {
-            return "Cannot be empty or whitespace only";
-          }
-          return true;
-        },
-      },
-    }}
-    render={({ field }) => (
-      <div style={{ margin: "16px 0" }}>
-        <ReactQuill
-          {...field}
-          value={field.value || ""}
-          onChange={(value) => field.onChange(value)}
-          placeholder={`Enter ${name}`}
-          modules={{
-            toolbar: [
-              [{ header: "1" }, { header: "2" }],
-              [{ list: "ordered" }, { list: "bullet" }],
-              ["bold", "italic", "underline"],
-              ["link"],
-              [{ color: [] }, { background: [] }],
-              ["image"],
-              [{ align: [] }],
-            ],
-          }}
-          style={{
-            height: "312px", 
-            marginBottom: "48px",
-          }}
-        />
-         {fieldError && (
-          <div
+      name={fieldName}
+      key={fieldName}
+      control={control}
+      defaultValue=""
+      rules={{
+        required: `${name} is required`,
+        // validate: {
+        //   notEmptyOrWhitespace: (value) => {
+        //     const textContent = value.replace(/<[^>]*>/g, "").trim();
+        //     if (!textContent) {
+        //       return "Cannot be empty or whitespace only";
+        //     }
+        //     return true;
+        //   },
+        // },
+      }}
+      render={({ field }) => (
+        <div style={{ margin: "16px 0" }}>
+          <ReactQuill
+            {...field}
+            value={field.value || ""}
+            onChange={(value) => field.onChange(value)}
+            placeholder={`Enter ${name}`}
             style={{
-              color: "#e53935",
-              marginTop: "8px",
-              fontSize: "13px",
-              marginLeft: "10px",
+              height: "312px",
+              marginBottom: "48px",
             }}
-          >
-            {fieldError.message ? fieldError.message : ""}
-          </div>
-        )}
-      </div>
-    )}
-  />
+            modules={QuillModules}
+          />
+          {fieldError && (
+            <div
+              style={{
+                color: "#e53935",
+                marginTop: "8px",
+                fontSize: "13px",
+                marginLeft: "10px",
+              }}
+            >
+              {fieldError.message ? fieldError.message : ""}
+            </div>
+          )}
+        </div>
+      )}
+    />
   );
 };
 
